b52b85125f6e5773d3c1132d3827213df4943044
blittable <blittable-bits@gmail.com>
2019-10-11 08:04:16 +0700

    basic tonic repo

---
+++ src/helloperf/client.rs
@@ -0,0 +1,20 @@
+pub mod hello_perf {
+    tonic::include_proto!("helloperf");
+}
+
+use hello_perf::{client::GreeterClient, HelloRequest};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = GreeterClient::connect("http://[::1]:50051")?;
+
+    let request = tonic::Request::new(HelloRequest {
+        name: "hello".into(),
+    });
+
+    let response = client.say_hello(request).await?;
+
+    println!("RESPONSE={:?}", response);
+
+    Ok(())
+}
